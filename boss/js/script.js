26070da6ac79eea34c4952e151e056f0790adc9d
rutschundflutsch <35765150+rutschundflutsch@users.noreply.github.com>
2018-05-24 16:52:50 +0200

    waterwheel

---
+++ boss/js/script.js
@@ -0,0 +1,46 @@
+function hideMobNav() {
+    $('.mobilebar, .burgericon').removeClass('open-nav');
+    $('.mobilebar').slideUp();
+}
+function showMobNav() {
+    $('.mobilebar, .burgericon').addClass('open-nav');
+    $('.mobilebar').slideDown();
+}
+
+(function($) {
+
+    //HEIGHT MOBILE-NAV///////////////////////////////////
+
+    var heightNav = $('.topnavbar').height();
+    var heightWindow = $(window).height();
+    var heightDropdown = heightWindow - heightNav;
+    console.log(heightDropdown);
+
+    $('.mobile-nav ul').css('height', heightWindow);
+
+    $( window ).resize(function() {
+        var heightNav = $('.topnavbar').height();
+        var heightWindow = $(window).height();
+        var heightDropdown = heightWindow - heightNav;
+        console.log(heightDropdown);
+
+        $('.mobile-nav ul').css('height', heightWindow);
+    });
+
+    ///////////////////////////////////////////////////
+
+
+
+    $('.burgericon').click(function () {
+        if($('.mobilebar').hasClass('open-nav')){
+            hideMobNav();
+        }else {
+            showMobNav();
+        }
+    });
+
+    $('.mobile-nav ul li').click(function () {
+        hideMobNav();
+    });
+
+}(jQuery));
